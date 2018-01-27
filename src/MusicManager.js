b381bfbb642753d216a4cfc3d1db15ee3fb6c9cb
Colin Creitz <ccreitz@fb.com>
2018-01-27 10:41:09 -0800

    renamed audio files to remove pesky spaces again

--- src/MusicManager.js
+++ src/MusicManager.js
@@ -1,3 +1,20 @@
 class MusicManager {
-	
+	constructor () {
+		game.clock.signal.add(MusicManager.HandleBeats);
+	}
+
+	static HandleBeats(beat, measure) {
+		switch (beat) {
+			case 0:
+			break;
+
+			case 1:
+			break;
+
+			case 2:
+			break;
+		}
+
+		
+	}
 }
