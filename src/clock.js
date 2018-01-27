d0cd3d21a98c189f678ad7e4ca57a3389751f1a4
Colin Creitz <ccreitz@fb.com>
2018-01-26 23:44:34 -0800

    Added an object clock to game.js, which emits a signal whose args are measure and beat number

---
+++ src/clock.js
@@ -0,0 +1,23 @@
+class Clock {
+	constructor() {
+		this.beatLength = 300; // in ms
+		this.measureLength = 3; // in beats
+		this.beat = 0;
+		this.measure = 0;
+		this.signal = new Phaser.Signal();
+		this.timer = game.time.create(false);
+		this.timer.loop(this.beatLength, this.beatHandler, this);
+		this.timer.start();
+	}
+
+	beatHandler() {
+		++this.beat;
+		if(this.beat % this.measureLength == 0) { // new measure
+			++this.measure;
+			this.beat = 0;
+		}
+		this.signal.dispatch(this.measure, this.beat);
+	}
+
+
+}
